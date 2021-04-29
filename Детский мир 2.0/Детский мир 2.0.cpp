4e271489085b56c2495b86f5f4b6596018930ba5
Даниил Ермошкин <mrcreeperdew@gmail.com>
2021-04-29 23:32:46 +0300

    Lists created

--- Детский мир 2.0/Детский мир 2.0.cpp
+++ Детский мир 2.0/Детский мир 2.0.cpp
@@ -24,6 +24,9 @@ struct list
 };
 
 vector <Mir> mir;
+const Mir NULLMIR = {
+    "9fd663c21v5asqe87s",-1,-1,-1,-1
+};
 
 void Nazvaniya(char* slovo, int switch_on)
 {
@@ -140,7 +143,91 @@ void printTable(vector<Mir> igrushka)
     printf("%c\n", 188);
     SetConsoleOutputCP(1251);
 }
+void printTableList(list* k)
+{
+    list* copyTable = k;
+    SetConsoleOutputCP(866);
+    printf("%c", 201);
+    for (int i = 0; i < 59; i++)
+    {
+        if ((i == 20) || (i == 30) || (i == 38) || (i == 48)) printf("%c", 203);
+        else printf("%c", 205);
+    }
+    printf("%c\n", 187);
 
+    printf("%c", 186);
+    SetConsoleOutputCP(1251);
+    printf("%-20s", "Название");
+    SetConsoleOutputCP(866);
+    printf("%c", 186);
+    SetConsoleOutputCP(1251);
+    printf("%-9s", "Цена");
+    SetConsoleOutputCP(866);
+    printf("%c", 186);
+    SetConsoleOutputCP(1251);
+    printf("%-7s", "Кол-во");
+    SetConsoleOutputCP(866);
+    printf("%c", 186);
+    SetConsoleOutputCP(1251);
+    printf("%-9s", "Мин.возр");
+    SetConsoleOutputCP(866);
+    printf("%c", 186);
+    SetConsoleOutputCP(1251);
+    printf("%-10s", "Макс.возр");
+    SetConsoleOutputCP(866);
+    printf("%c\n", 186);
+    printf("%c", 204);
+    for (int i = 0; i < 59; i++)
+    {
+        if ((i == 20) || (i == 30) || (i == 38) || (i == 48)) printf("%c", 206);
+        else printf("%c", 205);
+    }
+    printf("%c\n", 185);
+
+    while ( copyTable != NULL)
+    {
+        SetConsoleOutputCP(866);
+        printf("%c", 186);
+        SetConsoleOutputCP(1251);
+        Nazvaniya(copyTable->spisok.toy_name, 0);
+        SetConsoleOutputCP(866);
+        printf("%c", 186);
+        SetConsoleOutputCP(1251);
+        printf("%-9.2f", copyTable->spisok.price);
+        SetConsoleOutputCP(866);
+        printf("%c", 186);
+        SetConsoleOutputCP(1251);
+        printf("%-7i", copyTable->spisok.count);
+        SetConsoleOutputCP(866);
+        printf("%c", 186);
+        SetConsoleOutputCP(1251);
+        printf("%-9i", copyTable->spisok.min_age);
+        SetConsoleOutputCP(866);
+        printf("%c", 186);
+        SetConsoleOutputCP(1251);
+        printf("%-10i", copyTable->spisok.max_age);
+        SetConsoleOutputCP(866);
+        printf("%c\n", 186);
+        if (copyTable->next != NULL) {
+            printf("%c", 204);
+            for (int i = 0; i < 59; i++)
+            {
+                if ((i == 20) || (i == 30) || (i == 38) || (i == 48)) printf("%c", 206);
+                else printf("%c", 205);
+            }
+            printf("%c\n", 185);
+        }
+        copyTable = copyTable->next;
+    };
+    printf("%c", 200);
+    for (int i = 0; i < 59; i++)
+    {
+        if ((i == 20) || (i == 30) || (i == 38) || (i == 48)) printf("%c", 202);
+        else printf("%c", 205);
+    }
+    printf("%c\n", 188);
+    SetConsoleOutputCP(1251);
+}
 void getInfo()
 {
     // open file
@@ -175,50 +262,58 @@ void getInfo()
     // close file
     fclose(f);
 }
-
+Mir readElement()
+{
+    Mir kl;
+    cout << "Введите название игрушки: ";
+    getchar();
+    memset(kl.toy_name, 0, sizeof(char) * RAZMERCHAROV);
+    for (int i = 0; i < RAZMERCHAROV; i++)
+    {
+        kl.toy_name[i] = getchar();
+        if (kl.toy_name[i] == 10) break;
+    }
+    cout << "Введите цену игрушки: ";
+    cin >> kl.price;
+    if (kl.price < 0)
+    {
+        cout << "Ошибка ввода" << endl;
+        return NULLMIR;
+    }
+    cout << "Введите кол-во игрушек: ";
+    cin >> kl.count;
+    if (kl.count < 0)
+    {
+        cout << "Ошибка ввода" << endl;
+        return NULLMIR;
+    }
+    cout << "Введите минимальный возраст: ";
+    cin >> kl.min_age;
+    if (kl.min_age < 0)
+    {
+        cout << "Ошибка ввода" << endl;
+        return NULLMIR;
+    }
+    cout << "Введите максимальный возраст: ";
+    cin >> kl.max_age;
+    if ((kl.max_age < 0) || (kl.max_age < kl.min_age))
+    {
+        cout << "Ошибка ввода" << endl;
+        return NULLMIR;
+    }
+    return kl;
+}
 vector<Mir> readMir()
 {
+    
     vector <Mir> data;
     while (true)
     {
-        Mir kl;
-        cout << "Введите название игрушки: ";
-        getchar();
-        memset(kl.toy_name, 0, sizeof(char) * RAZMERCHAROV);
-        for (int i = 0; i < RAZMERCHAROV; i++)
-        {
-            kl.toy_name[i] = getchar();
-            if (kl.toy_name[i] == 10) break;
-        }
-        cout << "Введите цену игрушки: ";
-        cin >> kl.price;
-        if (kl.price < 0)
-        {
-            cout << "Ошибка ввода" << endl;
-            break;
-        }
-        cout << "Введите кол-во игрушек: ";
-        cin >> kl.count;
-        if (kl.count < 0)
-        {
-            cout << "Ошибка ввода" << endl;
-            break;
-        }
-        cout << "Введите минимальный возраст: ";
-        cin >> kl.min_age;
-        if (kl.min_age < 0)
-        {
-            cout << "Ошибка ввода" << endl;
-            break;
-        }
-        cout << "Введите максимальный возраст: ";
-        cin >> kl.max_age;
-        if ((kl.max_age < 0) || (kl.max_age < kl.min_age))
-        {
-            cout << "Ошибка ввода" << endl;
-            break;
-        }
-        data.push_back(kl);
+        
+        Mir buffer = readElement();
+        if (buffer.price == NULLMIR.price) break;
+        // TODO: Сделать проверку ошибок ввода
+        data.push_back(buffer);
         cout << "Хотите ли вы добавить еще игрушку? (y/n): ";
         char K;
         cin >> K;
@@ -443,8 +538,6 @@ void QS(vector <Mir>& igrushka, int left, int right)
     if (i < right)
         QS(igrushka, i, right);
 }
-
-
 void Pyatoe()
 {
     cout << "Хотите отсортировать файл? y/n: ";
@@ -467,6 +560,211 @@ void Pyatoe()
     }
 }
 
+list* init(Mir info, list* next)
+{
+    list* try1;
+    try1 = (list*)malloc(sizeof(list));
+    try1->spisok = info;
+    try1->next = next;
+    return(try1);
+}
+
+list* NewNoda(list* noda, Mir a)
+{
+    list* try2;
+    try2 = (list*)malloc(sizeof(list));
+    try2->spisok = a;
+    try2->next = noda->next;
+    noda->next = try2;
+    return(try2);
+
+}
+void Print(list* test)
+{
+    list* copy = test;
+    while (true)
+    {
+        Nazvaniya(copy->spisok.toy_name, 0);
+        //printf("%-20s", GetName(igrushka[i].toy_name));
+        printf("%-9.2f", copy->spisok.price);
+        printf("%-7i", copy->spisok.count);
+        printf("%-9i", copy->spisok.min_age);
+        printf("%-10i", copy->spisok.max_age);
+        cout << endl;
+        if (copy->next == NULL) break;
+        copy = copy->next;
+    }
+}
+void PushSort(list*& k, Mir inner)
+{
+    list* copy = k;
+    list* previous = NULL;
+    while (1)
+    {
+        if ((copy->spisok.price > inner.price) || (copy->next == NULL)) break;
+        else
+        {
+            previous = copy;
+            copy = copy->next;
+        }
+    }
+    if (copy->spisok.price > inner.price)
+    {
+        if (previous == NULL)
+        {
+            //Вставка в начало
+            list* a = init(inner, copy);
+            k = a;
+            return;
+        }
+        else
+        {
+            //Вставка в середину
+            NewNoda(previous, inner);
+            return;
+        }
+    }
+    //Вставка в конец
+    NewNoda(copy, inner);
+}
+void DeleteNoda(list* mainnoda, list* deletenoda)
+{
+    if (deletenoda == NULL) return;
+    list* copy = mainnoda;
+    while (true)
+    {
+        if (copy->next == deletenoda) break;
+        copy = copy->next;
+    }
+    copy->next = deletenoda->next;
+    free(deletenoda);
+}
+list* Search(char* k, list* MainNoda)
+{
+    list* copy = MainNoda;
+    while (true)
+    {
+        if ((copy->next == NULL) || (Sravnenie(copy->spisok.toy_name, k) == 0)) break;
+        copy = copy->next;
+    }
+    if (Sravnenie(copy->spisok.toy_name, k) == 0) return copy;
+    return NULL;
+}
+void WriteListToFile(list* k)
+{
+    int data_size = 1;
+    {
+        list* cope = k;
+        while (true)
+        {
+            if (cope->next != NULL)
+            {
+                data_size++;
+                cope = cope->next;
+            }
+            else break;
+        }
+    }
+    FILE* f;
+    fopen_s(&f, "Toys.txt", "wb");
+    fwrite(&data_size, sizeof(int), 1, f);
+    list* cope = k;
+    while (cope != NULL)
+    {
+        fwrite(&cope->spisok, sizeof(Mir), 1, f);
+        cope = cope->next;
+    }
+    fclose(f);
+    getInfo();
+}
+void WorkOnList()
+{
+    list* k = init(mir[0], NULL);
+    for (int i = 1; i < mir.size(); i++)
+    {
+        PushSort(k, mir[i]);
+    }
+    printTableList(k);
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int choice;
+    cout << "1.Добавить элемент\n2.Удалить элемент\n3.Выйти\nВыберите действие: ";
+    scanf_s("%i",&choice);
+    switch(choice)
+    {
+    case 1:
+    {
+        Mir buffer = readElement();
+        if (buffer.price == NULLMIR.price) break;
+        PushSort(k, buffer);
+        WriteListToFile(k);
+        break;
+    }
+   
+    case 2:
+    {
+        cout << "Введите название игрушки: ";
+        char name[RAZMERCHAROV];
+        scanf_s("%s", name, sizeof(name));
+        for (int i = 0; i < RAZMERCHAROV; i++)
+        {
+            if (name[i] == 0)
+            {
+                for (int j = i; j < RAZMERCHAROV; j++)
+                {
+                    name[j] = ' ';
+                }
+                break;
+            }
+        }
+        DeleteNoda(k, Search(name, k));
+        WriteListToFile(k);
+        break;
+    }
+    case 3:
+    {
+        break;
+    }
+    default: break;
+    }
+    return;
+    cout << "Введите название игрушки: ";
+    getchar();
+    SetConsoleCP(1251);
+    Mir opora;
+    char name[RAZMERCHAROV];
+    memset(name, 0, sizeof(char) * RAZMERCHAROV);
+    for (int i = 0; i < RAZMERCHAROV; i++)
+    {
+        name[i] = getchar();
+        if (name[i] == 10)
+        {
+            for (int j = i; j < RAZMERCHAROV; j++)
+            {
+                name[i] = ' ';
+                cout << j;
+            }
+            break;
+        }
+    }
+    DeleteNoda(k, Search(name, k));
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    Print(k);
+    int kolvo = 1;
+    {
+        list* cope = k;
+        while (true)
+        {
+            if (cope->next != NULL)
+            {
+                kolvo++;
+                cope = cope->next;
+            }
+            else break;
+        }
+    }
+}
 int main()
 {
     SetConsoleOutputCP(1251);
@@ -536,6 +834,7 @@ int main()
         }
         case 6:
         {
+            WorkOnList();
             break;
         }
         case 0:
